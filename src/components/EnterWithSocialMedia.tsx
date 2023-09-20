0679e60e2260f72e24ff5eac15bc6709e747cef5
tiagobbandrade <tiagobbandrade2704@gmail.com>
2023-09-20 13:59:42 -0300

    feat: create a enter with social media component (only style)

---
+++ src/components/EnterWithSocialMedia.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react'
+import { FcGoogle } from 'react-icons/fc'
+import { BsFacebook, BsApple } from 'react-icons/bs'
+
+export function EnterWithSocialMedia(){
+    return (
+        <div className='self-stretch flex items-center justify-center gap-5'>
+            <SocialMediaButton>
+                <FcGoogle />
+            </SocialMediaButton>
+            <SocialMediaButton>
+                <BsFacebook className='text-[#1877F2]'/>
+            </SocialMediaButton>
+            <SocialMediaButton>
+                <BsApple clasName='text-[#121212]' />
+            </SocialMediaButton>
+        </div>
+    )
+}
+
+function SocialMediaButton({children}: {children: ReactNode}){
+    return (
+        <button className='p-3 rounded-md border border-gray-100 shadow-sm shadow-[#12121210]'>
+            {children}
+        </button>
+    )
+}
